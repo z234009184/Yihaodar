ddf5ed935e6e762cad85b496971be501bd3b4a97
zhangguoliang <15004657729@163.com>
2018-01-31 09:55:47 +0800

    delete remote Pods dir

--- Pods/Target Support Files/Navigation-stack/Navigation-stack-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Navigation_stack : NSObject
-@end
-@implementation PodsDummy_Navigation_stack
-@end
--- Pods/Target Support Files/Pods-Yihaodar/Pods-Yihaodar-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Yihaodar : NSObject
-@end
-@implementation PodsDummy_Pods_Yihaodar
-@end
