6bd8ee4b753835f5f6b101cfccca16cc0d83e938
zhangguoliang <15004657729@163.com>
2018-02-15 00:01:22 +0800

    add view

--- Yihaodar/GLWrokTable/GLWorkTableController.swift
+++ Yihaodar/GLWrokTable/GLWorkTableController.swift
@@ -9,7 +9,7 @@
 import XLPagerTabStrip
 import HGPlaceholders
 import PullToRefreshKit
-
+import Spring
 
 // MARK: - GLWorkTableListCell
 /// GLWorkTableListCell
@@ -218,9 +218,45 @@ class GLWorkTableController: ButtonBarPagerTabStripViewController {
         navigationController?.pushViewController(searchVc, animated: true)
         
     }
+    
+    
     @objc func addAction(_ btn: UIButton) -> Void {
+        showAddView()
+    }
+    
+    func showAddView() -> Void {
+        tabBarController?.view.addSubview(maskView)
+        let addViewY = 167.0*UIScreen.main.bounds.size.height/667.0
+        let addViewH = UIScreen.main.bounds.size.height - addViewY
+        print(addView)
+        addView.frame = CGRect(x: 0, y: addViewY, width: maskView.bounds.size.width, height: addViewH)
+        maskView.addSubview(addView)
         
     }
+    
+    lazy var addView: DesignableView = {
+        let addV = Bundle.main.loadNibNamed("GLAddView", owner: nil, options: nil)?.first
+        guard let v = addV as? DesignableView else { return DesignableView()}
+        return v
+    }()
+    
+    
+    lazy var maskView: DesignableView = {
+        let mv = DesignableView(frame: (navigationController?.view.bounds)!)
+        mv.backgroundColor = UIColor(white: 0, alpha: 0.3)
+        let ges = UITapGestureRecognizer(target: self, action: #selector(GLTaskDetailPictureViewController.dismissCover(ges:)))
+        mv.addGestureRecognizer(ges)
+        return mv
+    }()
+    
+    @IBAction func closeAddView(_ sender: DesignableButton) {
+        maskView.removeFromSuperview()
+    }
+    
+    @objc func dismissCover(ges: UITapGestureRecognizer) {
+        ges.view?.removeFromSuperview()
+    }
+    
     
     /// PagerTabStripDataSource
     override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
