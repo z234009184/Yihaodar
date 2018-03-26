d953c506379479d89542cb7f67d9472147ca35e0
zhangguoliang <15004657729@163.com>
2018-03-26 18:31:13 +0800

    1

--- Yihaodar/GLTaskDetail/GLTaskDetailGPSViewController.swift
+++ Yihaodar/GLTaskDetail/GLTaskDetailGPSViewController.swift
@@ -39,8 +39,8 @@ class GLTaskDetailItemCell: UITableViewCell {
 }
 
 class GLTaskDetailBaseViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     open lazy var tableView: UITableView = {
-        
         let tableView = UITableView(frame: CGRect(x: 8, y: 0, width: view.frame.size.width-16, height: view.frame.size.height), style: UITableViewStyle.grouped)
         tableView.separatorStyle = UITableViewCellSeparatorStyle.none
         tableView.estimatedRowHeight = 40
@@ -114,6 +114,20 @@ class GLTaskDetailBaseViewController: UIViewController, UITableViewDelegate, UIT
         
     }
     
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        guard let parentVc = parent as? GLTaskDetailGPSViewController else {
+            return
+        }
+        let contentOffsetY = scrollView.contentOffset.y
+        if contentOffsetY <= 0 {
+            parentVc.topViewTopConstraint.constant = 0
+        } else if contentOffsetY > 0 && contentOffsetY <= 65 {
+            parentVc.topViewTopConstraint.constant = -contentOffsetY
+        } else {
+            parentVc.topViewTopConstraint.constant = -65
+        }
+    }
 }
 
 
@@ -130,9 +144,71 @@ class GL基本信息ViewController: GLTaskDetailBaseViewController, IndicatorInf
 }
 
 
+class GL评估信息ViewController: GLTaskDetailBaseViewController, IndicatorInfoProvider {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return IndicatorInfo(title: "评估信息")
+    }
+}
 
+class GL风险控制ViewController: GLTaskDetailBaseViewController, IndicatorInfoProvider {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return IndicatorInfo(title: "风险控制")
+    }
+}
 
+class GL尽职调查ViewController: GLTaskDetailBaseViewController, IndicatorInfoProvider {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return IndicatorInfo(title: "尽职调查")
+    }
+}
 
+class GL合同签约ViewController: GLTaskDetailBaseViewController, IndicatorInfoProvider {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return IndicatorInfo(title: "合同签约")
+    }
+}
+
+class GL资料附件ViewController: GLTaskDetailBaseViewController, IndicatorInfoProvider {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return IndicatorInfo(title: "资料附件")
+    }
+}
+
+class GL费用及放款ViewController: GLTaskDetailBaseViewController, IndicatorInfoProvider {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return IndicatorInfo(title: "费用及放款")
+    }
+}
 
 
 class GLTaskDetailGPSViewController: GLButtonBarPagerTabStripViewController {
@@ -156,7 +232,7 @@ class GLTaskDetailGPSViewController: GLButtonBarPagerTabStripViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "任务详情"
-        settings.style.buttonBarItemLeftRightMargin = 15
+//        settings.style.buttonBarItemLeftRightMargin = 8
     }
     
     
@@ -167,19 +243,35 @@ class GLTaskDetailGPSViewController: GLButtonBarPagerTabStripViewController {
     
     override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
         
-        return [basicVc, basicVc2, basicVc3]
+        return [vc1, vc2, vc3, vc4, vc5, vc6, vc7]
     }
     
-    lazy var basicVc = { () -> GL基本信息ViewController in
+    lazy var vc1 = { () -> GL基本信息ViewController in
         return GL基本信息ViewController()
     }()
     
-    lazy var basicVc2 = { () -> GL基本信息ViewController in
-        return GL基本信息ViewController()
+    lazy var vc2 = { () -> GL评估信息ViewController in
+        return GL评估信息ViewController()
     }()
     
-    lazy var basicVc3 = { () -> GL基本信息ViewController in
-        return GL基本信息ViewController()
+    lazy var vc3 = { () -> GL风险控制ViewController in
+        return GL风险控制ViewController()
+    }()
+    
+    lazy var vc4 = { () -> GL尽职调查ViewController in
+        return GL尽职调查ViewController()
+    }()
+    
+    lazy var vc5 = { () -> GL合同签约ViewController in
+        return GL合同签约ViewController()
+    }()
+    
+    lazy var vc6 = { () -> GL资料附件ViewController in
+        return GL资料附件ViewController()
+    }()
+    
+    lazy var vc7 = { () -> GL费用及放款ViewController in
+        return GL费用及放款ViewController()
     }()
     
 }
