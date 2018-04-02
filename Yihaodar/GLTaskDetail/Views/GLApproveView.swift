dc984c833395e866ee824d433df819fcfcc9523b
zhangguoliang <15004657729@163.com>
2018-04-02 10:56:51 +0800

    GLApproveView

---
+++ Yihaodar/GLTaskDetail/Views/GLApproveView.swift
@@ -0,0 +1,37 @@
+//
+//  GLApproveView.swift
+//  Yihaodar
+//
+//  Created by 张国梁 on 2018/4/2.
+//  Copyright © 2018年 Yihaodar. All rights reserved.
+//
+
+import Spring
+
+class GLApproveView: UIView {
+    
+    @IBOutlet weak var agreeBtn: UIButton!
+    @IBOutlet weak var declineBtn: UIButton!
+    private var lastBtn: UIButton?
+    @IBOutlet weak var remarksTextField: DesignableTextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    
+    
+    @IBAction func agreeOrDeclineBtnClick(_ sender: UIButton) {
+        sender.isSelected = true
+        lastBtn?.isSelected = false
+        lastBtn = sender
+    }
+    
+    @IBAction func submitBtnClick(_ sender: DesignableButton) {
+        
+        
+    }
+    
+    
+}
