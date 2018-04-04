89fceb5aeed61fba207a539cac5b865825c36011
zhangguoliang <15004657729@163.com>
2018-04-04 11:03:32 +0800

    fix

--- Pods/Target Support Files/Pods-CommonPods-Yihaodar/Pods-CommonPods-Yihaodar-umbrella.h
+++ Pods/Target Support Files/Pods-CommonPods-Yihaodar/Pods-CommonPods-Yihaodar-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_YihaodarVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_YihaodarVersionString[];
+FOUNDATION_EXPORT double Pods_CommonPods_YihaodarVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CommonPods_YihaodarVersionString[];
 
